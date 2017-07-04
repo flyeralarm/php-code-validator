9cb982fe19410ee630314f934684cf27b66b829c
Maximilian Grosch <m.grosch@flyeralarm.de>
2017-07-04 11:17:20 +0200

    Added NoInterfaceSuffixSniff

---
+++ Codesniffer/Sniffs/Flyeralarm/NoInterfaceSuffixSniff.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace PHP_CodeSniffer\Standards\Generic\Sniffs\Files;
+
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Files\File;
+
+class NoInterfaceSuffixSniff implements Sniff
+{
+    /**
+     * @return array
+     */
+    public function register()
+    {
+        return array(T_INTERFACE);
+
+    }
+
+
+    /**
+     * @param File $phpcsFile
+     * @param int $stackPtr
+     * @return void
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $interfaceName = $phpcsFile->getDeclarationName($stackPtr);
+
+        if (preg_match('/Interface$/i', $interfaceName) === 1) {
+            $phpcsFile->addError('Suffix "Interface" is not allowed', $stackPtr, 'InvalidInterfaceName');
+        }
+    }
+}
