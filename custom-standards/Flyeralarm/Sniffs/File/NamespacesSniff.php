cbef364db0aa06b9e2771b018838ee881993ba89
Maximilian Grosch <m.grosch@flyeralarm.de>
2017-08-11 08:47:18 +0200

    - Removed namespace case sniff (decision from 2017/08/10 http://confluence.flyeralarm/display/DEV/3.24+Technische+Abstimmungsrunde)

--- custom-standards/Flyeralarm/Sniffs/File/NamespacesSniff.php
+++ custom-standards/Flyeralarm/Sniffs/File/NamespacesSniff.php
@@ -25,14 +25,6 @@ class NamespacesSniff implements Sniff
         $tokens = $phpcsFile->getTokens();
         $ptr = -1;
         while($ptr = $phpcsFile->findNext(T_NS_SEPARATOR, $ptr + 1)) {
-            if (ctype_upper($tokens[$ptr + 1]['content'][0]) === false) {
-                $phpcsFile->addError(
-                    'Namespace declarations after vendor name must be in UpperCamelCase',
-                    $stackPtr,
-                    'NamespaceDeclarationWithInvalidCapitalization'
-                );
-            }
-
             if (strpos($tokens[$ptr + 1]['content'], '_') !== false) {
                 $phpcsFile->addError(
                     'Namespace declarations after vendor name must be in UpperCamelCase',
