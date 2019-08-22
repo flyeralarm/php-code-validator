0dd9e2daf7037e00bfc78e5670635e8a65731fad
fschmtt <frederik.schmitt1@gmx.de>
2019-08-22 08:30:43 +0200

    Allow mysqli, mysqli_driver, mysqli_result, mysqli_stmt, mysqli_sql_exception and mysqli_warning as return types

---
+++ tests/rules/doc/allowed/ReturnTypeMysqli.php
@@ -0,0 +1,50 @@
+<?php
+
+// @expectedPass
+
+namespace flyeralarm\Test;
+
+class FooTest
+{
+    /**
+     * @return mysqli
+     */
+    public function testMysqli()
+    {
+    }
+
+    /**
+     * @return mysqli_driver
+     */
+    public function testMysqliDriver()
+    {
+    }
+
+    /**
+     * @return mysqli_result
+     */
+    public function testMysqliResult()
+    {
+    }
+
+    /**
+     * @return mysqli_stmt
+     */
+    public function testMysqliStmt()
+    {
+    }
+
+    /**
+     * @return mysqli_sql_exception
+     */
+    public function testMysqliSqlException()
+    {
+    }
+
+    /**
+     * @return mysqli_warning
+     */
+    public function testMysqliWarning()
+    {
+    }
+}
