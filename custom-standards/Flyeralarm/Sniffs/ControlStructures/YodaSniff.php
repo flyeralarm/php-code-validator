6db90c824d8a5e2050841bf1147411787cfc91b0
s.duge <sven.duge@flyeralarm.com>
2024-11-25 14:09:14 +0100

    - fix for probably missing key in yoda condition - adding php 8.3

--- custom-standards/Flyeralarm/Sniffs/ControlStructures/YodaSniff.php
+++ custom-standards/Flyeralarm/Sniffs/ControlStructures/YodaSniff.php
@@ -2,8 +2,8 @@
 
 namespace Flyeralarm\CodingGuidelines\Flyeralarm\Sniffs\ControlStructures;
 
-use PHP_CodeSniffer\Sniffs\Sniff;
 use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
 
 class YodaSniff implements Sniff
 {
@@ -15,6 +15,7 @@ class YodaSniff implements Sniff
         return [T_IF, T_ELSEIF, T_WHILE];
     }
 
+
     /**
      * @param File $phpcsFile
      * @param int $stackPtr
@@ -25,7 +26,9 @@ class YodaSniff implements Sniff
         $tokens = $phpcsFile->getTokens();
 
         $startOfConditionPtr = $stackPtr;
-        $endPtr = $tokens[$startOfConditionPtr]['scope_opener'] + 1;
+        if (array_key_exists('scope_opener', $tokens[$startOfConditionPtr])) {
+            $endPtr = $tokens[$startOfConditionPtr]['scope_opener'] + 1;
+        }
 
         $logicalOperatorTokenIds = [T_BOOLEAN_AND, T_BOOLEAN_OR, T_LOGICAL_AND, T_LOGICAL_OR, T_LOGICAL_XOR];
         $scopeOpenerTokenIds = [T_OPEN_CURLY_BRACKET];
@@ -36,6 +39,7 @@ class YodaSniff implements Sniff
             $startOfConditionPtr = $endOfConditionPtr + 1;
         }
     }
+
 
     /**
      * @param File $phpcsFile
